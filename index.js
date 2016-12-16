b254d0bec388c835cea69ef4156076f90d6ccc1f
Florian Weber <florian@expensify.com>
2016-12-16 19:13:25 +0000

    Clean up some leftovers from v1

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ module.exports = {
         jquery: true,
         node: true
     },
-    rules: {
-        // Expensify legacy rules
-        //
-        // The following rules are actually supposed to be errors but we decided to
-        // switch them to warnings to not invalidate our current codebase. We should
-        // still try to get rid of these modifications at some point and turn them
-        // back in to errors.
-        //
-        'jsx-a11y/label-has-for': 1
-    },
     ecmaFeatures: {
         generators: true,
         objectLiteralDuplicateProperties: true,
