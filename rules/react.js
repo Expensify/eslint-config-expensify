0822ae562c7ad0046fb73f427cba98c4406fa992
Tim Golen <tgolen@expensify.com>
2020-01-31 13:56:04 -0800

    Update react.js

--- rules/react.js
+++ rules/react.js
@@ -27,6 +27,10 @@ module.exports = {
 
         // Rather then blindly enforcing destructuring assignments, we'll trust the author's best judgement on when
         // to make use of them, and when not; see https://github.com/Expensify/Style-Guide/pull/60 for more details
-        'react/destructuring-assignment': 'off'
+        'react/destructuring-assignment': 'off',
+        
+        'react/no-unsafe': ['error', {
+            checkAliases: true
+        }]
     }
 };
