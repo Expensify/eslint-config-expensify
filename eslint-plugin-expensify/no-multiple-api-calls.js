fc2d06ee08ddc04e5c166175f25f5dd8c5beb147
Carlos Martins <cmartins@expensify.com>
2022-05-26 14:04:55 -0600

    create rule

--- eslint-plugin-expensify/no-multiple-api-calls.js
+++ eslint-plugin-expensify/no-multiple-api-calls.js
@@ -1,8 +1,24 @@
-const lodashGet = require('lodash/get');
 const message = require('./CONST').MESSAGE.NO_MULTIPLE_API_CALLS;
 
 module.exports = {
-    create: context => ({
-        // check for multiple api calls in one method
-    }),
+    create(context) {
+        let count = 0;
+        function checkFunctionBody(node) {
+            if (node.name === 'deprecatedAPI' || node.name === 'API') {
+                count++;
+                if (count > 1) {
+                    context.report({
+                        node,
+                        message,
+                    });
+                }
+            }
+        }
+
+        return {
+            "FunctionDeclaration CallExpression Identifier": checkFunctionBody,
+            "FunctionExpression CallExpression Identifier": checkFunctionBody,
+            "ArrowFunctionExpression CallExpression Identifier": checkFunctionBody,
+        };
+    },
 };
