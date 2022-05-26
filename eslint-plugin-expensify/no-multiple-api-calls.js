0e1488636a76209ba3e766a7a25880de50d2f57c
Carlos Martins <cmartins@expensify.com>
2022-05-26 15:16:55 -0600

    update rule

--- eslint-plugin-expensify/no-multiple-api-calls.js
+++ eslint-plugin-expensify/no-multiple-api-calls.js
@@ -1,24 +1,40 @@
+const {isInActionFile} = require('./utils');
 const message = require('./CONST').MESSAGE.NO_MULTIPLE_API_CALLS;
 
 module.exports = {
     create(context) {
-        let count = 0;
         function checkFunctionBody(node) {
-            if (node.name === 'deprecatedAPI' || node.name === 'API') {
-                count++;
-                if (count > 1) {
-                    context.report({
-                        node,
-                        message,
-                    });
+            // This rule is scoped to the ./actions/ directory because we already have a rule
+            // preventing any API calls outside of actions.
+            if (!isInActionFile(context.getFilename())) {
+                return;
+            }
+
+            const tokens = context.getSourceCode().getTokens(node);
+            let hasCalledAPI = false;
+
+            for (let i = 0; i < tokens.length; i++) {
+                const token = tokens[i];
+                if (token.value !== 'deprecatedAPI' && token.value !== 'API') {
+                    continue;
                 }
+
+                if (!hasCalledAPI) {
+                    hasCalledAPI = true;
+                    continue;
+                }
+
+                context.report({
+                    node: token,
+                    message,
+                });
             }
         }
 
         return {
-            "FunctionDeclaration CallExpression Identifier": checkFunctionBody,
-            "FunctionExpression CallExpression Identifier": checkFunctionBody,
-            "ArrowFunctionExpression CallExpression Identifier": checkFunctionBody,
+            FunctionDeclaration: checkFunctionBody,
+            FunctionExpression: checkFunctionBody,
+            ArrowFunctionExpression: checkFunctionBody,
         };
     },
 };
