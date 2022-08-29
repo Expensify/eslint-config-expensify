9e046764b026fbed78601b814b8106f2bcc97038
Andrew Lor <andrewlor332@gmail.com>
2022-08-29 09:00:02 -0600

    code style changes to display-name-property rule

--- eslint-plugin-expensify/display-name-property.js
+++ eslint-plugin-expensify/display-name-property.js
@@ -18,50 +18,47 @@ module.exports = {
             }
 
             // Find a root level variable delcaration that matches the component name
-            const isFunctionComponent = _.find(node.body, n => n.type === 'VariableDeclaration'
+            const functionComponent = _.find(node.body, n => n.type === 'VariableDeclaration'
                     && _.get(n, 'declarations[0].id.name') === componentName
                     && ['FunctionExpression', 'ArrowFunctionExpression'].includes(_.get(n, 'declarations[0].init.type')));
 
             // Find a root level class delcaration that matches the component name
-            const isClassComponent = _.find(node.body, n => n.type === 'ClassDeclaration'
+            const classComponent = _.find(node.body, n => n.type === 'ClassDeclaration'
                 && _.get(n, 'id.name') === componentName);
 
-            if (!isFunctionComponent && !isClassComponent) {
+            if (_.isUndefined(functionComponent) && _.isUndefined(classComponent)) {
                 return;
             }
 
             // Find a root level assignment expression that matches <componentName>.displayName = ...
-            const hasDisplayNameProperty = _.find(node.body, bodyNode => bodyNode.type === 'ExpressionStatement'
+            const displayNameAssignment = _.find(node.body, bodyNode => bodyNode.type === 'ExpressionStatement'
                 && _.get(bodyNode, 'expression.type', '') === 'AssignmentExpression'
                 && _.get(bodyNode, 'expression.left.property.name', '') === 'displayName'
                 && _.get(bodyNode, 'expression.left.object.name', '') === componentName);
 
-            if (isFunctionComponent) {
-                // Allow function components with displayName set
-                if (hasDisplayNameProperty) {
-                    return;
-                }
-
-                // Ignore noop components, ie. const Test = () => null;
-                const component = isFunctionComponent;
-                const isNoopComponent = _.get(component, 'declarations[0].init.type') === 'ArrowFunctionExpression'
-                    && _.get(component, 'declarations[0].init.body.type') === 'Literal'
-                    && _.get(component, 'declarations[0].init.body.value') === null;
-
-                if (isNoopComponent) {
-                    return;
-                }
+            // Allow class components without displayName set
+            if (!_.isUndefined(classComponent) && _.isUndefined(displayNameAssignment)) {
+                return;
             }
 
-            // Allow class components without displayName set
-            if (isClassComponent && !hasDisplayNameProperty) {
+            // Allow function components with displayName set
+            if (!_.isUndefined(functionComponent) && !_.isUndefined(displayNameAssignment)) {
+                return;
+            }
+
+            const isNoopComponent = _.get(functionComponent, 'declarations[0].init.type') === 'ArrowFunctionExpression'
+                && _.get(functionComponent, 'declarations[0].init.body.type') === 'Literal'
+                && _.get(functionComponent, 'declarations[0].init.body.value') === null;
+
+            // Ignore noop components, ie. const Test = () => null;
+            if (isNoopComponent) {
                 return;
             }
 
             // Otherwise report
             context.report({
                 node,
-                message: isClassComponent ? DISPLAY_NAME_PROPERTY_CLASS : DISPLAY_NAME_PROPERTY_FUNCTION,
+                message: classComponent ? DISPLAY_NAME_PROPERTY_CLASS : DISPLAY_NAME_PROPERTY_FUNCTION,
             });
         },
     }),
