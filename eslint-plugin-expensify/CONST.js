a999396fe468cf7c7745873d50396b23389c48ae
Rushat Gabhane <rushatgabhane@gmail.com>
2022-08-30 00:54:56 +0530

    fix lint errors

--- eslint-plugin-expensify/CONST.js
+++ eslint-plugin-expensify/CONST.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 module.exports = {
     MESSAGE: {
         NO_API_IN_VIEWS: 'Do not call API directly outside of actions methods. Only actions should make API requests.',
@@ -11,8 +12,6 @@ module.exports = {
         PREFER_ONYX_CONNECT_IN_LIBS: 'Only call Onyx.connect() from inside a /src/libs/** file. React components and non-library code should not use Onyx.connect()',
         PREFER_UNDERSCORE_METHOD: 'Prefer \'_.{{method}}\' over the native function.',
         PREFER_STR_METHOD: 'Prefer \'Str.{{method}}\' over the native function.',
-
-        // eslint-disable-next-line max-len
         NO_MULTIPLE_API_CALLS: 'Do not call API multiple times in the same method. The API response should return all the necessary data in a single request, and API calls should not be chained together.',
         NO_CALL_ACTIONS_FROM_ACTIONS: 'Calling actions from inside other actions is forbidden. If an action needs to call another action combine the two actions into a singular API call instead.',
         NO_API_SIDE_EFFECTS_METHOD: 'Do not use makeRequestWithSideEffects.',
