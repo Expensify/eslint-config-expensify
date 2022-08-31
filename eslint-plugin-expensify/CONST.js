22a96f7cb69e2add2173af79253881173b9e56f2
Andrew Lor <andrewlor332@gmail.com>
2022-08-31 14:38:31 -0600

    Merge branch 'main' into fix-issue-7822-display-name-property-rule

--- eslint-plugin-expensify/CONST.js
+++ eslint-plugin-expensify/CONST.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 module.exports = {
     MESSAGE: {
         DISPLAY_NAME_PROPERTY_CLASS: 'Class components must not set the displayName property.',
@@ -13,8 +14,7 @@ module.exports = {
         PREFER_ONYX_CONNECT_IN_LIBS: 'Only call Onyx.connect() from inside a /src/libs/** file. React components and non-library code should not use Onyx.connect()',
         PREFER_UNDERSCORE_METHOD: 'Prefer \'_.{{method}}\' over the native function.',
         PREFER_STR_METHOD: 'Prefer \'Str.{{method}}\' over the native function.',
-
-        // eslint-disable-next-line max-len
+        PREFER_LOCALIZATION: 'Use Localize.translateLocal(\'i18n.key\') over hard coded messages.',
         NO_MULTIPLE_API_CALLS: 'Do not call API multiple times in the same method. The API response should return all the necessary data in a single request, and API calls should not be chained together.',
         NO_CALL_ACTIONS_FROM_ACTIONS: 'Calling actions from inside other actions is forbidden. If an action needs to call another action combine the two actions into a singular API call instead.',
         NO_API_SIDE_EFFECTS_METHOD: 'Do not use makeRequestWithSideEffects.',
