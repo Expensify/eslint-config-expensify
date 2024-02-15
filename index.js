cc43cc006f76ef1bc4290844268e6101cf650c71
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-15 10:43:26 +0100

    Fix formatting

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const rulesDirPlugin = require("eslint-plugin-rulesdir");
+const path = require('path');
+const rulesDirPlugin = require('eslint-plugin-rulesdir');
 
-rulesDirPlugin.RULES_DIR = path.resolve(__dirname, "eslint-plugin-expensify");
+rulesDirPlugin.RULES_DIR = path.resolve(__dirname, 'eslint-plugin-expensify');
 
 module.exports = {
-  plugins: ["@lwc/eslint-plugin-lwc", "eslint-plugin-es", "rulesdir"],
-  extends: [
-    "airbnb",
-    require.resolve("./rules/style"),
-    require.resolve("./rules/es6"),
-    require.resolve("./rules/react"),
-    "plugin:react-hooks/recommended",
-    require.resolve("./rules/expensify"),
-  ],
-  env: {
-    browser: true,
-    es6: true,
-    jquery: true,
-    node: true,
-  },
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      generators: true,
-      objectLiteralDuplicateProperties: true,
+    plugins: ['@lwc/eslint-plugin-lwc', 'eslint-plugin-es', 'rulesdir'],
+    extends: [
+        'airbnb',
+        require.resolve('./rules/style'),
+        require.resolve('./rules/es6'),
+        require.resolve('./rules/react'),
+        'plugin:react-hooks/recommended',
+        require.resolve('./rules/expensify'),
+    ],
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
+        node: true,
     },
-  },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            generators: true,
+            objectLiteralDuplicateProperties: true,
+        },
+    },
 };
