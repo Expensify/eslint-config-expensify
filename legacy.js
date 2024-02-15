62d889dbd1b92ea5c04d32441339e18be1826a92
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-15 11:15:05 +0100

    Fix formatting

--- legacy.js
+++ legacy.js
@@ -1,12 +1,15 @@
 module.exports = {
-  extends: ["airbnb-base/legacy", require.resolve("./rules/style")],
-  env: {
-    browser: true,
-    jquery: true,
-  },
-  rules: {
-    "no-param-reassign": "warn",
-    "no-underscore-dangle": "warn",
-    "no-unused-vars": "warn",
-  },
+    extends: [
+        'airbnb-base/legacy',
+        require.resolve('./rules/style'),
+    ],
+    env: {
+        browser: true,
+        jquery: true,
+    },
+    rules: {
+        'no-param-reassign': 'warn',
+        'no-underscore-dangle': 'warn',
+        'no-unused-vars': 'warn',
+    },
 };
