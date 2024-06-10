afb37c36ac3db2d929767307a75e09262bcfb721
Blazej Kustra <blazej.kustra@swmansion.com>
2024-06-10 15:46:21 +0200

    Document addNamedImport util

--- eslint-plugin-expensify/utils/imports.js
+++ eslint-plugin-expensify/utils/imports.js
@@ -1,3 +1,14 @@
+/**
+ * Adds a named import to the import statement or creates a new import statement if it doesn't exist.
+ *
+ * @param {Object} context - The ESLint rule context.
+ * @param {Object} fixer - The ESLint fixer object.
+ * @param {ASTNode} importNode - The import statement node.
+ * @param {string} importName - The name of the import.
+ * @param {string} importPath - The path of the import.
+ * @param {boolean} [importAsType=false] - Whether the import should be treated as a type import.
+ * @returns {Array} An array of fixes to be applied by the fixer.
+ */
 function addNamedImport(context, fixer, importNode, importName, importPath, importAsType = false) {
   const fixes = [];
   
