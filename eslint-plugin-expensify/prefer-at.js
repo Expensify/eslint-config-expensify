edb80f1ca31545d9e96ae17b1b315c33e4828402
Shridhar Goel <35566748+ShridharGoel@users.noreply.github.com>
2024-08-23 16:27:37 +0530

    Update

--- eslint-plugin-expensify/prefer-at.js
+++ eslint-plugin-expensify/prefer-at.js
@@ -1,6 +1,8 @@
 const { AST_NODE_TYPES, ESLintUtils } = require('@typescript-eslint/utils');
 const message = require('./CONST').MESSAGE.PREFER_AT;
 
+const { isLeftHandSide } = require('./utils/is-left-hand-side');
+
 module.exports = {
     meta: {
         fixable: 'code',
@@ -67,23 +69,19 @@ module.exports = {
             return indexExpression;
         }
 
-        function isAssignmentExpression(node) {
-            return node.parent && node.parent.type === AST_NODE_TYPES.AssignmentExpression && node.parent.left === node;
-        }
-
         function checkNode(node) {
             if (node.type === AST_NODE_TYPES.MemberExpression && node.property) {
                 if (!isArrayType(node.object)) {
                     return;
                 }
 
-                // Skip if the property is a method (like a.map)
+                // Skip if the property is a method (like a?.map)
                 if (node.parent && node.parent.type === AST_NODE_TYPES.CallExpression && node.parent.callee === node) {
                     return;
                 }
 
-                // Skip if the node is part of an assignment expression (like a[i] = 2)
-                if (isAssignmentExpression(node)) {
+                // Skip if the node is part of an assignment expression
+                if (isLeftHandSide(node)) {
                     return;
                 }
 
