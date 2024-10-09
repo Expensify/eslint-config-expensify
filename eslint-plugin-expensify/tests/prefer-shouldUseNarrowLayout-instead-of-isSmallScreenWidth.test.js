48f83ecca2b9e33116d821c94580d8c60fb9d08d
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-10-10 00:30:01 +0100

    Fix lint errors

--- eslint-plugin-expensify/tests/prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth.test.js
+++ eslint-plugin-expensify/tests/prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth.test.js
@@ -1,74 +1,73 @@
-const RuleTester = require("eslint").RuleTester;
-const rule = require("../prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth");
-const message =
-  require("../CONST").MESSAGE
+const RuleTester = require('eslint').RuleTester;
+const rule = require('../prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth');
+const message = require('../CONST').MESSAGE
     .PREFER_SHOULD_USE_NARROW_LAYOUT_INSTEAD_OF_IS_SMALL_SCREEN_WIDTH;
 
 const ruleTester = new RuleTester({
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-  },
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+    },
 });
 
 ruleTester.run(
-  "prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth",
-  rule,
-  {
-    valid: [
-      {
-        code: "const {shouldUseNarrowLayout} = useResponsiveLayout();",
-      },
-    ],
-    invalid: [
-      {
-        code: "const {isSmallScreenWidth} = useResponsiveLayout();",
-        errors: [
-          {
-            message,
-          },
+    'prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth',
+    rule,
+    {
+        valid: [
+            {
+                code: 'const {shouldUseNarrowLayout} = useResponsiveLayout();',
+            },
         ],
-      },
-      {
-        code: "const {isSmallScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();",
-        errors: [
-          {
-            message,
-          },
-        ],
-      },
-      {
-        code: `
+        invalid: [
+            {
+                code: 'const {isSmallScreenWidth} = useResponsiveLayout();',
+                errors: [
+                    {
+                        message,
+                    },
+                ],
+            },
+            {
+                code: 'const {isSmallScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();',
+                errors: [
+                    {
+                        message,
+                    },
+                ],
+            },
+            {
+                code: `
             const isSmallScreenWidth = useResponsiveLayout().isSmallScreenWidth;
             `,
-        errors: [
-          {
-            message,
-          },
-        ],
-      },
-      {
-        code: `
+                errors: [
+                    {
+                        message,
+                    },
+                ],
+            },
+            {
+                code: `
             const dimensions = useResponsiveLayout();
             const isSmallScreenWidth = dimensions.isSmallScreenWidth;
             `,
-        errors: [
-          {
-            message,
-          },
-        ],
-      },
-      {
-        code: `
+                errors: [
+                    {
+                        message,
+                    },
+                ],
+            },
+            {
+                code: `
             const dimensions = useResponsiveLayout();
             const {isSmallScreenWidth} = dimensions;
             `,
-        errors: [
-          {
-            message,
-          },
+                errors: [
+                    {
+                        message,
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  }
+    },
 );
