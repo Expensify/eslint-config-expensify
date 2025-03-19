e11c4071b34b00c8b70fc5d0e46f5a1a4d7ea5f7
Ni Wayan Devi <wayandevi813@gmail.com>
2025-03-19 20:38:07 +0800

    prefer-tokenized-search rule

--- eslint-plugin-expensify/prefer-tokenized-search.js
+++ eslint-plugin-expensify/prefer-tokenized-search.js
@@ -2,6 +2,110 @@ const lodashGet = require('lodash/get');
 const message = require('./CONST').MESSAGE.PREFER_TOKENIZED_SEARCH;
 
 /**
+ * Unwraps a ChainExpression if present.
+ * @param {Object} node
+ * @returns {Object}
+ */
+function extractCallExpression(node) {
+    return node && node.type === 'ChainExpression' ? node.expression : node;
+}
+
+/**
+ * Recursively checks if a node contains a call to toLowerCase.
+ * Skips the 'parent' property to avoid circular references.
+ * @param {Object} node
+ * @returns {Boolean}
+ */
+function containsToLowerCase(node) {
+    if (!node || typeof node !== 'object') return false;
+    if (node.type === 'CallExpression') {
+        const callExp = extractCallExpression(node);
+        if (lodashGet(callExp, 'callee.property.name') === 'toLowerCase') {
+            return true;
+        }
+    }
+    for (const key in node) {
+        if (key === 'parent') continue;
+        if (Object.prototype.hasOwnProperty.call(node, key)) {
+            const child = node[key];
+            if (Array.isArray(child)) {
+                for (const c of child) {
+                    if (containsToLowerCase(c)) {
+                        return true;
+                    }
+                }
+            } else if (child && typeof child === 'object') {
+                if (containsToLowerCase(child)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * Checks if a given call expression is suspicious:
+ *   - It calls "includes" on something that, somewhere in its chain,
+ *     calls toLowerCase.
+ * @param {Object} expr
+ * @returns {Boolean}
+ */
+function checkExpression(expr) {
+    if (!expr) {
+        return false;
+    }
+    // If the expression is a call expression, check if it's an includes call.
+    if (expr.type === 'CallExpression') {
+        const callExp = extractCallExpression(expr);
+        if (lodashGet(callExp, 'callee.property.name') === 'includes') {
+            const lowerCaseCall = lodashGet(callExp, 'callee.object');
+            if (containsToLowerCase(lowerCaseCall)) {
+                return true;
+            }
+        }
+    }
+    // If the expression is a logical expression (e.g. using ||), check both sides.
+    if (expr.type === 'LogicalExpression') {
+        return checkExpression(expr.left) || checkExpression(expr.right);
+    }
+    return false;
+}
+
+/**
+ * Recursively traverses a node's AST to find any suspicious call expression.
+ * Skips properties named 'parent' to avoid circular references.
+ * @param {Object} node
+ * @returns {Boolean}
+ */
+function findSuspiciousCall(node) {
+    if (!node || typeof node !== 'object') return false;
+    if (node.type === 'CallExpression' && checkExpression(node)) {
+        return true;
+    }
+    for (const key in node) {
+        if (key === 'parent') continue;
+        if (Object.prototype.hasOwnProperty.call(node, key)) {
+            const child = node[key];
+            if (Array.isArray(child)) {
+                for (const c of child) {
+                    if (findSuspiciousCall(c)) {
+                        return true;
+                    }
+                }
+            } else if (child && typeof child === 'object') {
+                if (findSuspiciousCall(child)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * Determines if the given CallExpression node is a filter call
+ * that uses includes on a lowercased value.
  * @param {Object} node
  * @returns {Boolean}
  */
@@ -15,22 +119,14 @@ function isUsingFilterIncludes(node) {
         return false;
     }
 
-    const filterBody = callback.body;
-
-    if (lodashGet(filterBody, 'callee.property.name') !== 'includes') {
-        return false;
+    // For expression-bodied arrow functions.
+    if (callback.body.type !== 'BlockStatement') {
+        const expr = extractCallExpression(callback.body);
+        return checkExpression(expr);
     }
 
-    const lowerCaseCall = lodashGet(filterBody, 'callee.object');
-
-    if (
-        lodashGet(lowerCaseCall, 'callee.property.name') === 'toLowerCase' ||
-        lodashGet(lowerCaseCall, 'callee.object.callee.property.name') === 'toLowerCase'
-    ) {
-        return true;
-    }
-
-    return false;
+    // For block-bodied arrow functions, recursively search the entire body.
+    return findSuspiciousCall(callback.body);
 }
 
 module.exports = {
--- eslint-plugin-expensify/tests/prefer-tokenized-search.test.js
+++ eslint-plugin-expensify/tests/prefer-tokenized-search.test.js
@@ -40,5 +40,60 @@ ruleTester.run('prefer-tokenized-search', rule, {
                 message: message,
             }],
         },
+        {
+            code: `const searchCategories = categoriesForSearch.filter((category) => {return category.name.toLowerCase().includes(searchValue.toLowerCase());});`,
+            errors: [{
+                message: message,
+            }],
+        },
+        {
+            code: `const filteredOptions = membersDetails.filter((option) => {return !!option.text?.toLowerCase().includes(searchValue) || !!option.alternateText?.toLowerCase().includes(searchValue);});`,
+            errors: [{
+                message: message,
+            }],
+        },
+        {
+            code: `const filtered = exportMenuItem?.data.filter((option) => {return option.value.toLowerCase().includes(searchText);});`,
+            errors: [{
+                message: message,
+            }],
+        },
+        {
+            code: `const filtered = array.filter(({ searchText }) => {return searchText.toLowerCase().includes(searchValue.toLowerCase());});`,
+            errors: [{
+                message: message,
+            }],
+        },
+        {
+            code: `
+                const filteredApprovers = debouncedSearchTerm !== ''
+                    ? approvers.filter((option) => {
+                        const searchValue = getSearchValueForPhoneOrEmail(debouncedSearchTerm);
+                        const isPartOfSearchTerm = !!option.text?.toLowerCase().includes(searchValue) || !!option.login?.toLowerCase().includes(searchValue);
+                        return isPartOfSearchTerm;
+                    })
+                    : approvers;
+            `,
+            errors: [{
+                message: message,
+            }],
+        },
+        {
+            code: `
+                const enabledSearchTags = enabledTagsWithoutSelectedOptions.filter((tag) => {
+                    return PolicyUtils.getCleanedTagName(tag.name.toLowerCase()).includes(searchValue.toLowerCase());
+                });
+            `,
+            errors: [{ message }],
+        },
+        {
+            code: `
+                const filteredOptions = [...formattedPolicyAdmins, ...formattedAuthorizedPayer].filter((option) => {
+                    const searchValue = OptionsListUtils.getSearchValueForPhoneOrEmail(searchTerm);
+                    return !!option.text?.toLowerCase().includes(searchValue) || !!option.login?.toLowerCase().includes(searchValue);
+                });
+            `,
+            errors: [{ message }],
+        },
     ],
 });
