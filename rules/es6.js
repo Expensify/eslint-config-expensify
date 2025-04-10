4128f6f53762bdcb91a53a319f0e36095d36eddb
rory <rory@expensify.com>
2025-04-10 09:21:33 -0700

    Include original error messages

--- rules/es6.js
+++ rules/es6.js
@@ -28,6 +28,20 @@ module.exports = {
         'no-invalid-this': 'error',
 
         // Override no-restricted-syntax to allow for...of loops
-        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+        'no-restricted-syntax': ['error',
+            {
+                selector: 'ForInStatement',
+                message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. '
+                    + 'Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize. It is also deprecated',
+            },
+        ],
     },
 };
