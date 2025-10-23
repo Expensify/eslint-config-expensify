30a35a719cb208d138fb59d4943ca0a6d3b3f1a9
NJ-2020 <ntt.london@gmail.com>
2025-10-23 08:03:36 +0700

    fix lint error

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ import react from './rules/react.js';
 import reactA11y from './rules/react-a11y.js';
 import es6 from './rules/es6.js';
 import style from './rules/style.js';
-import unicorn from './rules/unicorn.js';
 import expensify from './rules/expensify.js';
 import unicorn from './rules/unicorn.js';
 
@@ -38,7 +37,6 @@ const config = defineConfig([
     reactA11y,
     es6,
     style,
-    unicorn,
     reactHooks.configs['recommended-latest'],
     expensify,
     unicorn,
