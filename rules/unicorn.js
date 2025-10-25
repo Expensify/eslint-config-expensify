858fc1dd23c85536a4d0e382d57ac13b0004c35b
NJ-2020 <ntt.london@gmail.com>
2025-10-25 10:52:15 +0700

    remove some rules

--- rules/unicorn.js
+++ rules/unicorn.js
@@ -17,14 +17,6 @@ const config = defineConfig([{
         // Enforce that .find or .findLast are used instead of .filter
         // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-array-find.md
         'unicorn/prefer-array-find': 'error',
-
-        // Enforce to use .replaceAll instead of .replace when dealing over regex searches
-        // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-string-replace-all.md
-        'unicorn/prefer-string-replace-all': 'error',
-
-        // Enforce to use set.size instead of Array.from(set).length for better performance
-        // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-set-size.md
-        'unicorn/prefer-set-size': 'error',
     },
 }]);
 
